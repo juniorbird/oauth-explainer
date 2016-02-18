24664ef64782ed289b0520029f82caf350adb95e
Wade Armstrong <wade@wadearmstrong.com>
2016-02-17 17:46:19 -0800

    everything is labeled

--- oauth-draw.js
+++ oauth-draw.js
@@ -23,16 +23,25 @@ function advanceSlide() {
       // Show the login button and pass the client id, scope, redirect
       drawBrowserControl(50, 50, 150, 170, 50, 30, '#666');
       drawArrow(125, 225, 125, 378, 'v', 'r', 'new');
+      labelItem(140, 300, 'User requests auth', '#4A2218');
       drawArrow(180, 450, 800, 290, 'h', 'r', 'new');
+      labelItem(500, 400, 'App passes ClientID', '#4A2218');
       slide++;
       break;
 
     case 2:
 
       // Show the user auths
+
+      // reset
       drawArrow(125, 225, 125, 378, 'v', 'r', 'old');
       drawArrow(180, 450, 800, 290, 'h', 'r', 'old');
+      labelItem(140, 300, 'User requests auth', '#fff');
+      labelItem(500, 400, 'App passes ClientID', '#fff');
+
+      // draw new
       drawArrow(800, 290, 205, 120, 'h', 'l', 'new');
+      labelItem(350, 150, 'Slack confirms auth', '#4A2218');
       drawBrowserControl(50, 50, 150, 170, 50, 30, 'green');
       slide++;
       break;
@@ -40,10 +49,16 @@ function advanceSlide() {
     case 3:
 
       // get the code
-      drawArrow(800, 290, 205, 120, 'h', 'l', 'old');
-      drawArrow(205, 120, 800, 290, 'h', 'r', 'new');
-      drawArrow(800, 290, 180, 450, 'h', 'l', 'new');
 
+      // reset
+      drawArrow(800, 290, 205, 120, 'h', 'l', 'old');
+      labelItem(350, 150, 'Slack confirms auth', '#fff');
+
+      // draw new
+      drawArrow(205, 120, 800, 290, 'h', 'r', 'new');
+      labelItem(350, 150, 'User confirms auth', '#4A2218');
+      drawArrow(800, 290, 180, 450, 'h', 'l', 'new');
+      labelItem(500, 400, 'Slack passes one-time code', '#4A2218');
 
       slide++;
       break;
@@ -51,10 +66,18 @@ function advanceSlide() {
     case 4:
 
       // Exchange the code for a token
+
+      // reset
       drawArrow(205, 120, 800, 290, 'h', 'r', 'old');
       drawArrow(800, 290, 180, 450, 'h', 'l', 'old');
+      labelItem(350, 150, 'User confirms auth', '#fff');
+      labelItem(500, 400, 'Slack passes one-time code', '#fff');
+
+      // draw new
       drawArrow(180, 430, 800, 270, 'h', 'r', 'new');
+      labelItem(180, 330, 'App passes back one-time code and secret', '#4A2218');
       drawArrow(800, 310, 180, 470, 'h', 'l', 'new');
+      labelItem(500, 420, 'Slack passes persistent token', '#4A2218');
 
       slide++;
       break;
@@ -62,9 +85,16 @@ function advanceSlide() {
     case 5:
 
       // Do action on user's behalf
+
+      //reset
       drawArrow(180, 430, 800, 270, 'h', 'r', 'old');
       drawArrow(800, 310, 180, 470, 'h', 'l', 'old');
+      labelItem(180, 330, 'App passes back one-time code and secret', '#fff');
+      labelItem(500, 420, 'Slack passes persistent token', '#fff');
+
+      // draw new
       drawArrow(180, 450, 800, 290, 'h', 'r', 'new');
+      labelItem(500, 400, 'App uses token to make API call', '#4A2218');
 
       break;
   }
